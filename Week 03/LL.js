5cba95f879d02f8b0b66e2b2f241c2371f92f084
Yang Xinde Eric <ericyangxinde@gmail.com>
2020-10-19 17:15:48 +0800

    week3, chapter4&5

--- Week 03/LL.js
+++ Week 03/LL.js
@@ -2,7 +2,7 @@
  * @Author: Eric YangXinde
  * @Date: 2020-10-19 14:02:36
  * @LastModifiedBy: Eric YangXinde
- * @LastEditTime: 2020-10-19 16:03:02
+ * @LastEditTime: 2020-10-19 17:14:26
  * @Description:
  */
 const reg = /([0-9\.]+)|([ \t]+)|([\r\n]+)|(\+)|(\-)|(\*)|(\/)/g; // 分组
@@ -41,7 +41,125 @@ const tokenize = function* (expression) {
 		type: "EOF",
 	};
 };
-
+const source = [];
 for (let token of tokenize("6- 9 * 5 -3/2")) {
-	console.log(token);
+	// console.log(token);
+	if (token.type !== "Whitespace" && token.type !== "lineTerminator") {
+		source.push(token);
+	}
+}
+
+function Expression(tokens) {
+	if (
+		SOURCE[0].type === "AdditiveExpression" &&
+		source[1] &&
+		source[1].type === "EOF"
+	) {
+		let node = {
+			type: "Expression",
+			children: [source.shift(), source.shift()], // 最后变成单个根节点
+		};
+		source.unshift(node);
+		return node;
+	}
+	AdditiveExpression(source);
+	return Expression(source);
+}
+function AdditiveExpression(source) {
+	if (source[0].type === "MultiplicativeExpression") {
+		let node = {
+			type: "AdditiveExpression",
+			children: [source[0]],
+		};
+		source[0] = node;
+		return AdditiveExpression(source);
+	}
+	if (
+		source[0].type === "AdditiveExpression" &&
+		source[1] &&
+		source[1].type === "+"
+	) {
+		let node = {
+			type: "AdditiveExpression",
+			operator: "+",
+			children: [],
+		};
+		node.children.push(source.shift());
+		node.children.push(source.shift());
+		MultiplicativeExpression(source); // 处理source中的非终结符
+		node.children.push(source.shift());
+		source.unshift(node);
+		return AdditiveExpression(source);
+	}
+	if (
+		source[0].type === "AdditiveExpression" &&
+		source[1] &&
+		source[1].type === "-"
+	) {
+		let node = {
+			type: "AdditiveExpression",
+			operator: "-",
+			children: [],
+		};
+		node.children.push(source.shift());
+		node.children.push(source.shift());
+		MultiplicativeExpression(source);
+		node.children.push(source.shift());
+		source.unshift(node);
+		return AdditiveExpression(source);
+	}
+
+	if (source[0].type === "AdditiveExpression") {
+		return source[0];
+	}
+
+	MultiplicativeExpression(source); // 遇到不认识的先调乘法，比如：Number
+	return AdditiveExpression(source);
+}
+
+function MultiplicativeExpression(source) {
+	// 单独一个Number看成是0次的乘法
+	if (source[0].type === "Number") {
+		let node = {
+			type: "MultiplicativeExpression",
+			children: [source[0]],
+		};
+		source[0] = node;
+		return MultiplicativeExpression(source);
+	}
+	if (
+		source[0].type === "MultiplicativeExpression" &&
+		source[1] &&
+		source[1].type === "*"
+	) {
+		let node = {
+			type: "MultiplicativeExpression",
+			operator: "*",
+			children: [],
+		};
+		node.children.push(source.shift());
+		node.children.push(source.shift());
+		node.children.push(source.shift());
+		source.unshift(node);
+		return MultiplicativeExpression(source);
+	}
+	if (
+		source[0].type === "MultiplicativeExpression" &&
+		source[1] &&
+		source[1].type === "/"
+	) {
+		let node = {
+			type: "MultiplicativeExpression",
+			operator: "/",
+			children: [],
+		};
+		source[0] = node;
+		return MultiplicativeExpression(source);
+	}
+
+	if (source[0].type === "MultiplicativeExpression") {
+		return source[0];
+	}
+
+	return MultiplicativeExpression(source);
 }
